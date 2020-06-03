c80d0530fd2bdad8bba581b1b20df1e9ec9840c0
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-03 16:07:39 -0700

    add alert box to prevent empty contact and confirm dialogue for deleting
    Co-authored-by: Matt McFarland <mrmcfarland@gmail.com>
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,22 @@ $(document).ready(function() {
     const emailAddressInput = $("#emailAddress").val();
     const phoneNumberInput = $("#phoneNumber").val();
     const streetAddressInput = $("#streetAddress").val();
-
-    $("ul#contactsList").append(`<li class="contactHead well"><strong>${contactNameInput}</strong></li>`);
-    $("li.contactHead:last-of-type").append(`<p class="contactInfo">Email Address: ${emailAddressInput}<br>Phone Number: ${phoneNumberInput}<br>Street Address: ${streetAddressInput}<br></p>`);
-    $("li.contactHead:last-of-type").click(function() {
+    
+    if (contactNameInput === "") {
+      alert("Contact name cannot be empty");
+    } else {
+      $("ul#contactsList").append(`<li class="contactHead well"><strong>${contactNameInput}</strong></li>`);
+      $("li.contactHead:last-of-type").append(`<p class="contactInfo">Email Address: ${emailAddressInput}<br>Phone Number: ${phoneNumberInput}<br>Street Address: ${streetAddressInput}<br></p>`);
+      $("li.contactHead:last-of-type").click(function() {
       $(this).children("p").toggle();
-    });
-    $("li.contactHead:last-of-type").append(`<p class="delete">Delete Contact?</p>`);
-    $("li.contactHead:last-of-type > p.delete").click(function() {
-      $(this).parent().remove();
-    });
+      });
+      $("li.contactHead:last-of-type").append(`<p class="delete">Delete Contact?</p>`);
+      $("li.contactHead:last-of-type > p.delete").click(function() {
+        const deleteConfirm = confirm("Really delete?");
+        if (deleteConfirm === true) {
+          $(this).parent().remove();
+        };
+      });
+    };
   });
 })
